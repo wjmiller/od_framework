c64d2f994ca12a07f5a1074420950d1595a6eeec
Will Miller <will.miller@Wills-MacBook-Pro-2.local>
2019-05-29 12:48:57 -0400

    my final changes for region highlight

---
+++ components/activities/custom/region_highlight.vue
@@ -0,0 +1,488 @@
+<template>
+<b-row class="region-highlight">
+  <b-col lg="9">
+    <candle-chart v-bind:candles="candles"
+                  v-bind:regions="chartRegions"
+                  v-bind:time-label="timeLabel"
+                  v-bind:time-label-interval="timeLabelInterval"
+                  v-bind:height="chartHeight"
+                  v-bind:force-range="priceRange"
+                  v-bind:price-display="priceDisplay"
+                  v-bind:price-width="priceWidth"
+                  v-bind:candle-width="candleWidth"
+                  v-bind:candle-spacing="candleSpacing"
+                  v-bind:candle-highlight="candleHighlight">
+    </candle-chart>
+  </b-col>
+  <b-col lg="3">
+    <v-select class="region-select"
+              v-if="chartRegions.length > 0"
+              v-model="region"
+              v-bind:options="chartRegions"
+              v-on:input="setRegion"
+              v-bind:placeholder="'Edit region'"
+              v-bind:searchable="false"
+              v-bind:clearable="false">
+      <template slot="option"
+                slot-scope="option">
+        {{ option.label }} <span class="dot"></span>
+      </template>
+
+    </v-select>
+    <div class="slider-cont"
+         v-if="region">
+      <h3>Edit Location</h3>
+
+      <span v-html=""></span>
+      <vue-slider v-model="region.range"
+                  v-on:change="updateRegion"
+                  v-bind:direction="'ltr'"
+                  v-bind:min="0"
+                  v-bind:max="candles.length - 1"
+                  v-bind:tooltip="'none'"
+                  v-bind:enable-cross="false"
+                  v-bind:min-range="1"
+                  v-bind:interval="sliderInterval">
+      </vue-slider>
+    </div>
+
+    <b-btn class="add-region"
+           v-if="addRegionButton"
+           v-on:click="openAddRegion">Add New Region</b-btn>
+  </b-col>
+  <div v-if="addingRegion"
+       class="add-region-cont">
+    <div class="add-region-bg"></div>
+    <div class="add-region">
+      <div class="add-region-header"><span>Select Region Type</span></div>
+      <div class="add-region-body">
+        <v-select v-model="regionType"
+                  v-bind:options="regionTypes"
+                  v-bind:placeholder="'Choose type'"
+                  v-on:input="cancelError"
+                  v-bind:searchable="false"
+                  v-bind:clearable="false">
+          <template slot="option"
+                    slot-scope="option">
+            {{ option.label }}
+          </template>
+
+        </v-select>
+        <div class="add-region-btns">
+          <b-btn v-on:click="cancelRegion">Cancel</b-btn>
+          <b-btn v-on:click="addRegion">Create</b-btn>
+        </div>
+        <div class="add-region-error"
+             v-if="addError">Please select a region type.</div>
+      </div>
+    </div>
+  </div>
+</b-row>
+</template>
+
+<script>
+import CandleChart from '~/components/activities/candle_chart'
+
+export default {
+  components: {
+    CandleChart
+  },
+  data() {
+    return {
+      region: null,
+      regionsArr: [ ...this.regions ],
+      addError: false,
+      addingRegion: false,
+      regionType: null,
+      regionTypes: [
+        {
+          label: 'RBR'
+        },
+        {
+          label: 'RBD'
+        },
+        {
+          label: 'DBR'
+        },
+        {
+          label: 'DBD'
+        },
+        {
+          label: 'Correction'
+        },
+        {
+          label: 'Impulsion'
+        },
+        {
+          label: 'Gap'
+        }
+      ]
+    }
+  },
+  props: {
+    candles: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    regions: {
+      type: Array,
+      default () {
+        return []
+      }
+    },
+    chartHeight: {
+      type: Number,
+      default: 260
+    },
+    timeLabel: {
+      type: String
+    },
+    timeLabelInterval: {
+      type: Number
+    },
+    priceRange: {
+      type: Array,
+      default () {
+        return [ 0, 40 ]
+      }
+    },
+    priceDisplay: {
+      type: Number,
+      default: 5
+    },
+    priceWidth: {
+      type: Number,
+      default: 60
+    },
+    candleWidth: {
+      type: Number,
+      default: 20
+    },
+    candleSpacing: {
+      type: Number,
+      default: 40
+    },
+    candleHighlight: {
+      type: Boolean,
+      default: false
+    },
+    sliderInterval: {
+      type: Number,
+      default: 1
+    },
+    addRegionButton: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    setRegion( val ) {
+      console.log( this.chartRegions[ val.index ] )
+      this.region = this.chartRegions[ val.index ]
+    },
+    updateRegion() {
+      const newRegions = [ ...this.regionsArr ]
+      newRegions[ this.region.index ] = this.region
+      this.regionsArr = newRegions
+    },
+    addRegion() {
+      if ( this.regionType ) {
+        this.regionsArr = [ ...this.regionsArr, {
+          label: this.regionType.label
+        } ]
+        this.addingRegion = false
+      } else {
+        this.addError = true
+      }
+    },
+    openAddRegion() {
+      this.addingRegion = true
+    },
+    cancelRegion() {
+      this.addError = false
+      this.regionType = null
+      this.addingRegion = false
+    },
+    cancelError() {
+      this.addError = false
+    }
+  },
+  computed: {
+    chartRegions() {
+      return this.regionsArr.map( ( item, ix ) => {
+        return {
+          label: item.label,
+          index: ix,
+          range: [ item.range ? item.range[ 0 ] : 0, item.range ? item.range[ 1 ] : 0 ]
+        }
+      } )
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+// -----------------------------------------------------
+// Import Variables
+// -----------------------------------------------------
+
+@import '~assets/scss/variables.scss';
+@import '~assets/scss/mixins.scss';
+
+.region-highlight {
+    position: relative;
+    padding-bottom: 20px;
+
+    .region-select {
+        display: block;
+        width: 100%;
+        min-width: 210px;
+        margin-bottom: 30px;
+    }
+
+    .add-region {
+        width: 100%;
+        //margin-top: 30px;
+    }
+
+    .add-region-cont {
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 1005;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+
+        .add-region-bg {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: $dark-blue;
+            opacity: 0.4;
+            z-index: 1005;
+        }
+
+        .add-region {
+            width: 90%;
+            max-width: 450px;
+            background: lighten($dark-blue, 3%);
+            border: 1px solid lighten($dark-blue, 17%);
+            align-self: center;
+            margin-top: 160px;
+            z-index: 1010;
+
+            @media(min-width: 768px) {
+                margin-top: 50px;
+            }
+
+            .add-region-header {
+                font-size: 1.4rem;
+                font-weight: 600;
+                text-align: center;
+                padding: 20px 25px;
+                background: lighten($dark-blue, 8%);
+                border-bottom: 1px solid lighten($dark-blue, 17%);
+            }
+
+            .add-region-body {
+                display: flex;
+                flex-direction: column;
+                justify-content: space-around;
+                padding: 30px 25px 10px;
+                font-size: 1.15rem;
+                font-weight: 600;
+
+                .v-select {
+                    margin-bottom: 25px;
+                }
+
+                .add-region-btns {
+                    align-self: center;
+                    margin-bottom: 15px;
+
+                    button:first-child {
+                        @include button-color($gray, 6%);
+                        margin-right: 15px;
+                    }
+                }
+
+                .add-region-error {
+                    color: $red;
+                    font-size: 1rem;
+                    text-align: center;
+                    margin-bottom: 10px;
+
+                }
+            }
+        }
+    }
+
+    .slider-cont {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        margin-bottom: 50px;
+        height: 90px;
+
+        @media(min-width: 768px) {
+            height: 80px;
+        }
+
+        .vue-slider,
+        h3,
+        span {
+            align-self: center;
+        }
+
+        h3,
+        span {
+            font-size: 1.4rem;
+            text-transform: uppercase;
+        }
+
+        span {
+            margin-bottom: 35px;
+            font-size: 1.1rem;
+        }
+
+        .vue-slider {
+            display: block;
+            padding: 0 !important;
+            width: calc(100% - 40px) !important;
+
+            .vue-slider-rail {
+                height: 12px;
+                border-radius: 0;
+                background: lighten($dark-blue, 5%);
+                border: 1px solid lighten($dark-blue, 15%);
+            }
+
+            .vue-slider-process {
+                border-radius: 6px;
+                background: none !important;
+            }
+
+            .vue-slider-dot {
+                width: 40px;
+
+                .vue-slider-dot-handle {
+                    width: 40px;
+                    height: 40px;
+                    position: relative;
+                    top: -14px;
+                    left: -13px;
+                    text-align: center;
+                    line-height: 40px;
+                    z-index: 1000;
+                    font-size: 36px;
+                    transform: rotate(90deg);
+
+                    &:before {
+                        content: "\e90f";
+                        font-family: "custom-icons";
+
+                        color: $dark-blue;
+                    }
+
+                }
+            }
+        }
+    }
+
+    .start-btn {
+        width: 100%;
+        margin-top: 50px;
+    }
+}
+
+.light {
+
+    .region-highlight {
+
+        .add-region-cont {
+            .add-region-bg {
+                background: #fff;
+                opacity: 0.6;
+            }
+
+            .od-loader {
+                .spinner {
+                    > div {
+                        background: $light-header-color;
+                    }
+                }
+            }
+
+            .add-region {
+                background: #fcfcfc;
+                border: 1px solid #bbbbbb;
+
+                .add-region-header {
+                    background: rgba(0,0,0,0.03);
+                    border-bottom: 1px solid #bbbbbb;
+                    color: $light-header-color;
+                }
+
+                .add-region-body {
+                    color: darken($light-text-color, 10%);
+
+                    .v-select {
+                        margin-bottom: 30px;
+
+                        .vs__dropdown-toggle {
+                            border: 1px solid #bbbbbb;
+                        }
+
+                        .vs__dropdown-menu {
+                            border: 1px solid #bbbbbb;
+
+                            li {
+                                color: $dark-blue;
+
+                                &:hover {
+                                    background: $light-header-color;
+                                    color: #ffffff;
+                                }
+                            }
+                        }
+
+                        .vs__dropdown-option--highlight {
+                            background: $light-header-color;
+                            color: #ffffff;
+                        }
+                    }
+                }
+            }
+        }
+
+        .slider-cont {
+            h3 {
+                color: $light-header-color;
+            }
+
+            .vue-slider {
+                .vue-slider-dot-handle {
+                    background: $light-header-color;
+                    box-shadow: none;
+                    &:before {
+                        color: #fff;
+                    }
+                }
+                .vue-slider-rail {
+                    background: rgba(0,0,0,0.03);
+                    border: 1px solid #bbbbbb;
+                }
+            }
+        }
+    }
+
+}
+</style>
--- pages/test/candlestickmaker/index.vue
+++ pages/test/candlestickmaker/index.vue
@@ -22,29 +22,42 @@
                     v-bind:candle-width="8"
                     v-bind:candle-spacing="12"
                     v-bind:detail-pane="true"
-                    v-bind:detail-position="'side'">
+                    v-bind:detail-position="'side'"
+                    v-bind:candle-highlight="false">
       </candle-chart>
     </b-col>
   </b-row>
   <b-row>
     <b-col cols="9">
-      <div ref="sketchpad" width="600" height="400"></div>
+      <div ref="sketchpad"
+           width="600"
+           height="400"></div>
     </b-col>
     <b-col cols="3">
       <b-button-group>
-      <b-button v-on:click="clearPad">Clear</b-button>
-      <b-button v-on:click="calcCandleData">Switch Data</b-button>
-    </b-button-group>
-    <div>
-    <label for="range-1"># of Candles</label>
-    <b-form-input id="range-1" v-model.number="candleCount" type="range" min="0" max="100" step="5"></b-form-input>
-    <div class="mt-2">Value: {{ candleCount }}</div>
-    </div>
-    <div>
-    <label for="range-1">Random Wick Size</label>
-    <b-form-input id="range-1" v-model.number="wickVariable" type="range" min="0" max="10" step="1"></b-form-input>
-    <div class="mt-2">Value: {{ wickVariable }}</div>
-    </div>
+        <b-button v-on:click="clearPad">Clear</b-button>
+        <b-button v-on:click="calcCandleData">Switch Data</b-button>
+      </b-button-group>
+      <div>
+        <label for="range-1"># of Candles</label>
+        <b-form-input id="range-1"
+                      v-model.number="candleCount"
+                      type="range"
+                      min="0"
+                      max="100"
+                      step="5"></b-form-input>
+        <div class="mt-2">Value: {{ candleCount }}</div>
+      </div>
+      <div>
+        <label for="range-1">Random Wick Size</label>
+        <b-form-input id="range-1"
+                      v-model.number="wickVariable"
+                      type="range"
+                      min="0"
+                      max="10"
+                      step="1"></b-form-input>
+        <div class="mt-2">Value: {{ wickVariable }}</div>
+      </div>
     </b-col>
   </b-row>
 </b-container>
@@ -61,8 +74,13 @@ export default {
       candleCount: 30,
       wickVariable: 1,
       chartLines: [ 20, 30 ],
-      candles: [{open: 1, close: 2, high: 3, low: 4}],
-      priceRange: [25, 29]
+      candles: [ {
+        open: 1,
+        close: 2,
+        high: 3,
+        low: 4
+      } ],
+      priceRange: [ 25, 29 ]
     }
   },
   methods: {
@@ -78,102 +96,129 @@ export default {
       const numOfCandles = this.candleCount
       const padData = this.pad.toJSON()
 
-      const pointData = padData && padData.strokes && padData.strokes[0] ? padData.strokes[0].points : []
+      const pointData = padData && padData.strokes && padData.strokes[ 0 ] ? padData.strokes[ 0 ].points : []
 
-      if (pointData.length > 0) {
+      if ( pointData.length > 0 ) {
         const invertYCanvas = y => 1 - y
-        const decimals2 = val => Math.floor(val * 100)/100
-        const candles = Math.min(numOfCandles, pointData.length)
+        const decimals2 = val => Math.floor( val * 100 ) / 100
+        const candles = Math.min( numOfCandles, pointData.length )
 
-        const ranges = pointData.reduce((memo, item) => {
-          memo.x[0] = Math.min(memo.x[0], item.x)
-          memo.x[1] = Math.max(memo.x[1], item.x)
-          memo.y[0] = Math.min(memo.y[0], invertYCanvas(item.y))
-          memo.y[1] = Math.max(memo.y[1], invertYCanvas(item.y))
+        const ranges = pointData.reduce( ( memo, item ) => {
+          memo.x[ 0 ] = Math.min( memo.x[ 0 ], item.x )
+          memo.x[ 1 ] = Math.max( memo.x[ 1 ], item.x )
+          memo.y[ 0 ] = Math.min( memo.y[ 0 ], invertYCanvas( item.y ) )
+          memo.y[ 1 ] = Math.max( memo.y[ 1 ], invertYCanvas( item.y ) )
           return memo
-        }, {x: [1, 0], y: [1, 0]})
+        }, {
+          x: [ 1, 0 ],
+          y: [ 1, 0 ]
+        } )
 
-        const xDiff = ranges.x[1] - ranges.x[0]
-        const yDiff = ranges.y[1] - ranges.y[0]
-        const priceDiff = this.priceRange[1] - this.priceRange[0]
+        const xDiff = ranges.x[ 1 ] - ranges.x[ 0 ]
+        const yDiff = ranges.y[ 1 ] - ranges.y[ 0 ]
+        const priceDiff = this.priceRange[ 1 ] - this.priceRange[ 0 ]
 
-        const convertYToPrice = y => this.priceRange[0] + decimals2(priceDiff * ((invertYCanvas(y) - ranges.y[0])/yDiff))
+        const convertYToPrice = y => this.priceRange[ 0 ] + decimals2( priceDiff * ( ( invertYCanvas( y ) - ranges.y[ 0 ] ) / yDiff ) )
 
         //(canvasHeight - y)
 
 
 
-        const xValues = Array(numOfCandles)
+        const xValues = Array( numOfCandles )
           .fill()
-          .map((item, ix) => ranges.x[0] + (xDiff/candles*ix))
+          .map( ( item, ix ) => ranges.x[ 0 ] + ( xDiff / candles * ix ) )
 
-        const lineData = pointData.reduce((memo, item, ix, arr) => {
-          const nextInArr = arr[ix + 1] || arr[ix]
-          memo.push({
-            start: {x: item.x, y: item.y},
-            end: {x: nextInArr.x, y: nextInArr.y}
-          })
+        const lineData = pointData.reduce( ( memo, item, ix, arr ) => {
+          const nextInArr = arr[ ix + 1 ] || arr[ ix ]
+          memo.push( {
+            start: {
+              x: item.x,
+              y: item.y
+            },
+            end: {
+              x: nextInArr.x,
+              y: nextInArr.y
+            }
+          } )
           return memo
-        }, []);
+        }, [] );
 
-        const reducedLines = lineData.reduce((memo, item) => {
-          const inMemo = memo.find(m => m.start.x === item.start.x)
-          if (inMemo) {
-            inMemo.end = {x: item.end.x, y: convertYToPrice(item.end.y)}
+        const reducedLines = lineData.reduce( ( memo, item ) => {
+          const inMemo = memo.find( m => m.start.x === item.start.x )
+          if ( inMemo ) {
+            inMemo.end = {
+              x: item.end.x,
+              y: convertYToPrice( item.end.y )
+            }
           } else {
-            memo.push({start: {x: item.start.x, y: convertYToPrice(item.start.y)}, end: {x: item.end.x, y: convertYToPrice(item.end.y)}})
+            memo.push( {
+              start: {
+                x: item.start.x,
+                y: convertYToPrice( item.start.y )
+              },
+              end: {
+                x: item.end.x,
+                y: convertYToPrice( item.end.y )
+              }
+            } )
           }
           return memo
-        }, [])
+        }, [] )
 
 
-        xValues.forEach(item => {
-        	const inReduced = reducedLines.find(rl => rl.start.x === item)
-        	if (!inReduced) {
-              const itemToSplit = reducedLines.find(rl => rl.start.x < item && rl.end.x > item)
-              const endValues = {x: itemToSplit.end.x, y: itemToSplit.end.y}
-              const splitValue = {
-                x: item,
-                y: itemToSplit.start.y + (itemToSplit.end.x - item)/(itemToSplit.end.x - itemToSplit.start.x)*(itemToSplit.end.y - itemToSplit.start.y)
-              }
-              itemToSplit.end = splitValue
-              reducedLines.push({start: splitValue, end: endValues})
+        xValues.forEach( item => {
+          const inReduced = reducedLines.find( rl => rl.start.x === item )
+          if ( !inReduced ) {
+            const itemToSplit = reducedLines.find( rl => rl.start.x < item && rl.end.x > item )
+            const endValues = {
+              x: itemToSplit.end.x,
+              y: itemToSplit.end.y
             }
-        });
+            const splitValue = {
+              x: item,
+              y: itemToSplit.start.y + ( itemToSplit.end.x - item ) / ( itemToSplit.end.x - itemToSplit.start.x ) * ( itemToSplit.end.y - itemToSplit.start.y )
+            }
+            itemToSplit.end = splitValue
+            reducedLines.push( {
+              start: splitValue,
+              end: endValues
+            } )
+          }
+        } );
 
-        reducedLines.sort((a, b) => a.start.x - b.start.x)
+        reducedLines.sort( ( a, b ) => a.start.x - b.start.x )
 
 
-        const candleData = xValues.reduce((memo, item, ix, arr) => {
-          const highValue = arr[ix + 1] || (ranges.x[1] + 1)
-          const lines = reducedLines.filter(rl => rl.start.x >= item && rl.start.x < highValue)
-          const lineEnds = lines.map(l => l.end.y)
-          memo.push({
-            open: decimals2(lines[0].start.y),
-            close: decimals2(lines[lines.length - 1].end.y),
-            high: decimals2(Math.max(...lineEnds, lines[0].start.y)),
-            low: decimals2(Math.min(...lineEnds, lines[0].start.y)),
-          })
+        const candleData = xValues.reduce( ( memo, item, ix, arr ) => {
+          const highValue = arr[ ix + 1 ] || ( ranges.x[ 1 ] + 1 )
+          const lines = reducedLines.filter( rl => rl.start.x >= item && rl.start.x < highValue )
+          const lineEnds = lines.map( l => l.end.y )
+          memo.push( {
+            open: decimals2( lines[ 0 ].start.y ),
+            close: decimals2( lines[ lines.length - 1 ].end.y ),
+            high: decimals2( Math.max( ...lineEnds, lines[ 0 ].start.y ) ),
+            low: decimals2( Math.min( ...lineEnds, lines[ 0 ].start.y ) ),
+          } )
           return memo
-        }, [])
+        }, [] )
 
-        if (this.wickVariable > 0) {
-          candleData.forEach(candle => {
-            const candleDiff = (candle.high - candle.low)
-            const candlePricePercent = 1 - candleDiff/priceDiff
-            const candleWickSize = this.wickVariable/20
-            const wickHigh = decimals2(candle.high + candlePricePercent * Math.random() * candleWickSize)
-            const wickLow = decimals2(candle.low - candlePricePercent * Math.random() * candleWickSize)
-            if (Math.random() >= 0.5) {
+        if ( this.wickVariable > 0 ) {
+          candleData.forEach( candle => {
+            const candleDiff = ( candle.high - candle.low )
+            const candlePricePercent = 1 - candleDiff / priceDiff
+            const candleWickSize = this.wickVariable / 20
+            const wickHigh = decimals2( candle.high + candlePricePercent * Math.random() * candleWickSize )
+            const wickLow = decimals2( candle.low - candlePricePercent * Math.random() * candleWickSize )
+            if ( Math.random() >= 0.5 ) {
               candle.high = wickHigh
             }
-            if (Math.random() >= 0.5) {
+            if ( Math.random() >= 0.5 ) {
               candle.low = wickLow
             }
-          })
+          } )
         }
 
-        if (this.candles.length > 0) {
+        if ( this.candles.length > 0 ) {
           this.candles = candleData
         }
       }
@@ -198,16 +243,14 @@ export default {
   mounted() {
     var el = this.$refs.sketchpad;
     this.pad = new sketchpad(
-    el,
-    {
-      line: {
+      el, {
+        line: {
           color: '#000000',
           size: 5
-      }
-    });
+        }
+      } );
   }
 }
-
 </script>
 
 <style lang="scss" scoped>
